50504ec4ceb2b22658101f3fa19785908e64d88c
DavonHicks123 <47585618+DavonHicks123@users.noreply.github.com>
2019-03-15 23:25:40 -0400

    Add files via upload

---
+++ DateTest.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+public class DateTest {
+
+	public static void main(String[] args) {
+			ArrayList<Date>dates=new ArrayList<Date>();
+			dates.add(new Date(4,13,1743));//Jefferson
+			dates.add(new Date(2,22,1732));//Washington
+			dates.add(new Date(3,16,1751));//Madison
+			dates.add(new Date(10,30,1735));//Adams
+			dates.add(new Date(4,28,1758));//Monroe
+			
+			System.out.println("birthdays="+dates);
+			Collections.sort(dates);
+			System.out.println("birthdays="+dates);
+
+		
+
+	}
+
+}
